b8501db58003883c2d8dc67eab48da39297899de
Marcin Guszpit <marcin.guszpit@gmail.com>
2019-05-10 15:06:51 +0200

    webpack+react init

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1> Witaj, świecie !</h1>,
+    document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SassLintPlugin = require('sass-lint-webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+let cleanOptions = {
+    root: path.join(__dirname, ''),
+    verbose: true,
+    dry: false
+};
+
+const srcPath = './src/';
+const outputPath = './build/';
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: path.join(__dirname, srcPath + 'app.js')
+    },
+    output: {
+        path: path.join(__dirname, outputPath),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [{ loader: 'babel-loader' }, { loader: 'eslint-loader' }]
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [{ loader: MiniCssExtractPlugin.loader },
+                    { loader: 'css-loader' }, { loader: 'sass-loader' }]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                loader: 'html-loader'
+
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(cleanOptions),
+        new SassLintPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'My App',
+            template: path.join(__dirname, srcPath + 'index.html')
+        }),
+        new MiniCssExtractPlugin({
+            path: path.join(__dirname, outputPath),
+            filename: 'style.css'
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, outputPath),
+        compress: true,
+        port: 4580
+    }
+};
