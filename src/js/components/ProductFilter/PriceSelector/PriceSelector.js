8d77036004d580c40b1be40633d60417e4120252
Marcin Guszpit <marcin.guszpit@gmail.com>
2019-07-24 15:17:25 +0200

    changes

--- src/js/components/ProductFilter/PriceSelector/PriceSelector.js
+++ src/js/components/ProductFilter/PriceSelector/PriceSelector.js
@@ -6,32 +6,89 @@ export class PriceSelector extends Component {
         super(props);
         this.pointerMinRef = React.createRef();
         this.pointerMaxRef = React.createRef();
+        this.backgroundBeamRef = React.createRef();
+        this.minMaxWidth = null;
         this.state = {
             min: props.min,
-            max: props.max
+            x1: props.min,
+            x2: props.max,
+            max: props.max,
+            isMouseDown: false
         };
         this.changeMinValue = (event) => {
-            console.log('changed min value');
-            this.setState({
-                min: event.target.value
-            });
+            let val = event.target.value;
+            if (val) {
+                val = limitValues(val,this.state.min, this.state.max);
+                this.setPointerPosition(this.pointerMinRef.current, val);
+                this.setState({
+                    x1: val
+                });
+            }
         };
         this.changeMaxValue = (event) => {
             console.log('changed max value');
             this.setState({
-                max: event.target.value
+                x2: event.target.value
             });
         }
 
     }
 
+    setMinMaxWitdth() {
+        const beamElem = this.backgroundBeamRef.current;
+        this.minMaxWidth = getMinMaxWidth(beamElem, 10);
+    }
+
     componentDidMount() {
-        this.pointerMaxRef.current.addEventListener('click',()=>{
-            console.log('max');
+        this.setMinMaxWitdth();
+
+        const minElem = this.pointerMinRef.current;
+        this.setMouseUpDownLeaveListener(minElem);
+        this.setPointerPosition(minElem,this.state.x1);
+
+        const maxElem = this.pointerMaxRef.current;
+        this.setMouseUpDownLeaveListener(maxElem);
+        this.setPointerPosition(maxElem,this.state.x2);
+
+        minElem.addEventListener('mousemove', (mouseEvent) => {
+            if (this.state.isMouseDown) {
+                const x = limitValues(mouseEvent.clientX, this.minMaxWidth.minWidth, this.minMaxWidth.maxWidth);
+                this.setState({
+                   x1: position2Value(x,this.minMaxWidth.minWidth,
+                       this.minMaxWidth.maxWidth,this.state.min,this.state.max)
+                });
+                const newStyleLeft = getStyleLeft(x, this.minMaxWidth);
+                if (newStyleLeft) {
+                    minElem.style.left = newStyleLeft;
+                }
+            }
         });
-        this.pointerMinRef.current.addEventListener('click', ()=>{
-            console.log('min');
-        })
+    }
+
+    setPointerPosition(element, position) {
+        const elemLeftStyle = getStyleLeft(value2Position(position, this.state.min, this.state.max,
+            this.minMaxWidth.minWidth, this.minMaxWidth.maxWidth), this.minMaxWidth);
+        element.style.left = elemLeftStyle;
+    }
+
+    setMouseUpDownLeaveListener(element) {
+        element.addEventListener('mouseleave', () => {
+            this.setState({
+                isMouseDown: false
+            });
+        });
+
+        element.addEventListener('mousedown', () => {
+            this.setState({
+                isMouseDown: true
+            });
+        });
+
+        element.addEventListener('mouseup', () => {
+            this.setState({
+                isMouseDown: false
+            });
+        });
     }
 
     render() {
@@ -39,14 +96,14 @@ export class PriceSelector extends Component {
             <div className="price-filter">
                 <h4 className="small-title">Price</h4>
                 <div className="field">
-                    <div className="beam">
+                    <div className="beam" ref={this.backgroundBeamRef}>
                     </div>
                     <div className="pointer pointer-min" ref={this.pointerMinRef}></div>
                     <div className="pointer pointer-max" ref={this.pointerMaxRef}></div>
                 </div>
                 <div className="inputs-row">
-                    <input className="input-value" type="text" value={this.state.min} onChange={this.changeMinValue}/>
-                    <input className="input-value" type="text" value={this.state.max} onChange={this.changeMaxValue}/>
+                    <input className="input-value" type="text" value={this.state.x1} onChange={this.changeMinValue}/>
+                    <input className="input-value" type="text" value={this.state.x2} onChange={this.changeMaxValue}/>
                 </div>
 
             </div>
@@ -59,11 +116,19 @@ PriceSelector.propTypes = {
     max: PropTypes.number.isRequired
 };
 
-function changeValue(x, min, max) {
-    return reverseTransformation(x, min, max);
+
+function position2Value(position, minPostion, maxPosition, valueMin, valueMax) {
+    const value = Math.round(valueMin + (valueMax - valueMin) * (position - minPostion) / (maxPosition - minPostion));
+    return value;
 }
 
-function limitValues(x,min,max) {
+function value2Position(value, valueMin, valueMax, minPosition, maxPosition) {
+    const position = Math.round(minPosition +
+        (maxPosition - minPosition) * (value - valueMin) / (valueMax - valueMin));
+    return position;
+}
+
+function limitValues(x, min,max) {
     if (x < min) {
         return min;
     }
@@ -73,28 +138,18 @@ function limitValues(x,min,max) {
     return x;
 }
 
-function reverseTransformation(yValue, yMin, yMax, minValue, maxValue){
-    const xValue = Math.round(minValue + (maxValue - minValue) * (yValue - yMin) / (yMax - yMin));
-    return xValue;
-}
-
-function transformation(xValue, yMin, yMax, minValue,maxValue) {
-    const yValue = Math.round(yMin +
-        (yMax - yMin) * (xValue - minValue) / (maxValue - minValue));
-    return yValue;
-}
-
-/*
-private setMinMaxWidth() {
-    const rect = this.htmlFieldElement.getBoundingClientRect();
-    this.minWidth = Math.floor(rect.left + (this.pointerWidth / 2));
-    this.maxWidth = Math.ceil(rect.left + rect.width - (this.pointerWidth / 2));
-}
-
-private changePointerPositionAndFillBeamLength(x: number) {
-    if (x >= this.minWidth && x <= this.maxWidth) {
-        const len = x - this.minWidth;
-        this.htmlPointerElement.style.left = len + 'px';
-        this.htmlBeamFillElement.style.width = len + 'px';
+function getMinMaxWidth(element, pointerWidth) {
+    const rect = element.getBoundingClientRect();
+    return {
+        minWidth: Math.floor(rect.left + (pointerWidth / 2)),
+        maxWidth: Math.ceil(rect.left + rect.width - (pointerWidth / 2))
     }
-}*/
+}
+
+function getStyleLeft(x, minMaxWidth) {
+    if (x >= minMaxWidth.minWidth && x <= minMaxWidth.maxWidth) {
+        const len = x - minMaxWidth.minWidth;
+        return len + 'px';
+    }
+    return null;
+}
